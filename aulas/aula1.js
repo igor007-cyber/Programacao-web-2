7883cb58bfde65765b3de75527241167a2b320e5
igor007_cyber <igor.ramalho007@gmail.com>
2023-10-03 23:09:58 -0300

    aulas

---
+++ aulas/aula1.js
@@ -0,0 +1,32 @@
+alert('Hello')
+alert('World')
+alert(3 +
+    1
+    + 2)
+
+alert('hello')
+[1,2].forEach(alert);
+
+let mensage;
+mensage = "oi"
+alert(mensage)
+
+let user = 'john', age = 25, message = 'hello'; // linha unica, mas nao recomendado
+
+/*
+Forma correta:
+
+let user = 'john'
+let age = 25
+let message = 'hello'
+*/
+
+if(true){
+    var test = true;
+}
+alert(test);
+
+if(true){
+    let test = true;
+}
+alert(test);
---
+++ aulas/aula2.js
@@ -0,0 +1,36 @@
+var x = 1
+console.log(X + " " + Y);
+var y = 2;
+
+var num1 = 3;
+num2 = 4;
+console.log(num1 + " " + num2);
+var num2;
+
+a = 'Cran';
+b= 'berry';
+console.log(a + "" + b);
+var a,b;
+
+typeof 10n
+
+let idade = prompt('Qual é sua idade');
+alert(`sua idade é ${idade} `);
+
+let acabar = confirm('voce tem certeza disso?');
+
+let idade1 = prompt('Idade?',18);
+let mensagem = (idade1 < 3)? 'oi,bebe':
+    (idade < 18) ? 'oi':
+    (idade < 100) ? 'ola':
+    'voce ainda esta por aqui';
+
+alert(mensagem)
+
+let i = 3
+while(i) alert(i--)
+
+let a = 0
+for(; a < 3; a++){
+    alert(a);
+}

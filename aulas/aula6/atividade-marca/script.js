02c032bb4bb2a1b51e568b1fee5283ae9d3aa027
igor007_cyber <igor.ramalho007@gmail.com>
2023-11-13 22:39:54 -0300

    atividades

--- aulas/aula6/atividade-marca/script.js
+++ aulas/aula6/atividade-marca/script.js
@@ -1,21 +1,28 @@
 const getMarcas = () => {
-    const tarefas =  fetch(`https://raw.githubusercontent.com/filippofilip95/car-logos-dataset/master/logos/data.json`)
+    const tarefas =  fetch(`https://raw.githubusercontent.com/filippofilip95/car-logos-dataset/master/logos/data.json`);
 
     tarefas
         .then(resposta => resposta.json())
         .then(marcas => {
-            const ul = document.createElement('ul')
+            const ul = document.createElement('ul');
             marcas.forEach(marca => {
-                const li = document.createElement("li")
-                const logo = document.createElement('img')
-                logo.src = marca.image?.optimized
-                li.appendChild(logo)
-                ul.appendChild(li)
-                console.log(marca)
-            })
-            document.body.appendChild(ul)
+                const li = document.createElement("li");
+
+                // Create an element for the brand name
+                const NomeMarca = document.createElement('p');
+                NomeMarca.textContent = marca.name;
+                li.appendChild(NomeMarca);
+
+                const logo = document.createElement('img');
+                logo.src = marca.image?.optimized;
+                li.appendChild(logo);
+
+                ul.appendChild(li);
+            });
+            document.body.appendChild(ul);
         })
-        .catch(erro => console.log(erro))
-}   
-const btnMarcas = document.querySelector('#marcas')
-btnMarcas.addEventListener('click', getMarcas)
+        .catch(erro => console.log(erro));
+};
+
+const btnMarcas = document.querySelector('#marcas');
+btnMarcas.addEventListener('click', getMarcas);
